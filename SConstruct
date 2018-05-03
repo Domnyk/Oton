992178b46a4507c8a47a34869ea702604db37524
Unknown <dominik.trusinski@gmail.com>
2018-05-03 19:51:33 +0200

    Add libraries to support boost log library

--- SConstruct
+++ SConstruct
@@ -3,9 +3,12 @@ import os
 env = Environment()
 
 env.Append(LIBS = [
-	'boost_system',
+    'boost_system',
+    'boost_log_setup-mt',
+    'boost_log-mt',
+    'boost_thread-mt',
 	'boost_filesystem',
-	'boost_serialization',
+	'boost_serialization'
 ])
 
 source 	= env.Glob('./src/*.cpp')
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
+#define BOOST_LOG_DYN_LINK
+
 #include <iostream>
 #include <boost/asio.hpp>
-
+#include <boost/log/trivial.hpp>
 #include "tcp_server.hpp"
 #include "tcp_connection.hpp"
 
@@ -10,6 +12,9 @@ int main() {
 	try {
     	boost::asio::io_context io_context;
     	tcp_server server(io_context);
+
+    	BOOST_LOG_TRIVIAL(info) << "Server is running on port: " << server.get_port();
+
     	io_context.run();
   	} catch (std::exception& e) {
     	std::cerr << e.what() << std::endl;
